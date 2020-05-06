42d7bef8278e367c271ffca218056829e4457306
shwethaGitHub-spec <62240096+shwethaGitHub-spec@users.noreply.github.com>
2020-05-06 20:16:14 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,9 +1,9 @@
 class Ground {
     constructor(x,y,width,height) {
-      var options = {
+      var options_ground = {
           isStatic: true
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.body = Bodies.rectangle(x,y,width,height,options_ground);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
